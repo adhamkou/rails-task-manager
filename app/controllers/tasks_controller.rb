105e077d09db3005eff5f6b0ff619d0eb4442245
Adham Al Koukou <adhamalkoukou@gmail.com>
2023-02-21 14:37:06 -0500

    chlg 2 dun

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,37 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params_task)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  # how are they related, whe i press edit why does it work
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params_task)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def params_task
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/tasks', to: 'tasks#index'
   # Defines the root path route ("/")
+  get 'tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
   get '/tasks/:id', to: 'tasks#show', as: :task
   # root "articles#index"
 end
